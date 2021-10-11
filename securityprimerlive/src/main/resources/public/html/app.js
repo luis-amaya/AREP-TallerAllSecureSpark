650700ae3ef7fac336689a3d13b5d9b23203c522
Luis Amaya <luis.amaya-o@mail.escuelaing.edu.co>
2021-10-10 23:43:10 -0500

    Terminado, Video añadido

--- securityprimerlive/src/main/resources/public/html/app.js
+++ securityprimerlive/src/main/resources/public/html/app.js
@@ -1,5 +1,6 @@
 function ingresar() {
     var user = document.getElementById("user").value;
     var password = document.getElementById("password").value;
-    location.href = 'https://localhost:4567/service?user=' + user + '&passw=' + password;
+    location.href = 'https://localhost:4567/service?user=' + user + '&passw=' +
+        password;
 }
